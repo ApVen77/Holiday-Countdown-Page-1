29543e3b89db79085dc3a6a5007f9dbc88e193eb
kevmccall <kev_mccall@yahoo.com>
2020-12-09 16:29:54 -0700

    added html, styling, and JS

---
+++ fortune.js
@@ -0,0 +1,6 @@
+function tellFortune(numChild, jobTitle, partner, location) {
+    var future = "You will be a " + jobTitle + " in " + location + " and married to a " + partner + " with " + numChild + " kids";
+    console.log(future);
+}
+
+tellFortune(3, "teacher", "Network Engineer", "Denver");
---
+++ puppycalc.js
@@ -0,0 +1,23 @@
+function calculateDogAge(age) {
+    //1 year for dogs is equivalent to  7 human years
+    var dogYears= 7 * age;
+    console.log("Your dog is " + dogYears + " years old in dog years!!");
+}
+document.getElementById("dogage").innerHTML= humanyears [age]; 
+
+
+
+// calculateDogAge(1);
+// calculateDogAge(2);
+// calculateDogAge(3);
+// calculateDogAge(4);
+// calculateDogAge(5);
+// calculateDogAge(6);
+// calculateDogAge(7);
+// calculateDogAge(8);
+// calculateDogAge(9);
+// calculateDogAge(10);
+// calculateDogAge(11);
+// calculateDogAge(12);
+// calculateDogAge(13);
+
