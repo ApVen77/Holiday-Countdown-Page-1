46bdcd3588b351efae8e548bf9ee8ea4537ec274
kevmccall <kev_mccall@yahoo.com>
2020-12-16 14:58:49 -0700

    added age verifier

--- puppycalc.js
+++ puppycalc.js
@@ -7,3 +7,22 @@ function calculateDogAge(age) {
 //Targets human years ID in HTML and applies variable dog years
 document.getElementById("humanyears").innerHTML= dogYears;
 };
+
+//Age CalculatorS
+var checkName = window.prompt("Enter your name:")
+var checkAge = window.prompt("Enter your age:")
+
+if (checkAge < 18) {
+    window.alert("You are under " + checkAge + ": and therefore not authorized to access this page.");
+    location.replace("http://www.google.com");
+}
+
+else if (checkAge > 18 && checkAge < 100) {
+    window.alert("You are old enough to visit the site. Welcome " + checkName + "!");
+
+}
+
+else {
+    window.alert("You must be honest about your age," + checkName + ".");
+}
+
